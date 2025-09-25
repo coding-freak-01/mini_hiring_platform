7134cbb987302045a3e5b0b69c1697d68b908498
coding-freak-01 <ankitassuman700@gmail.com>
2025-09-25 23:49:56 +0530

    initialised vite setup and tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-blue-800 bg-amber-300'>
+      Hello Its Ankita Suman
+    </div>
+  )
+}
+
+export default App
