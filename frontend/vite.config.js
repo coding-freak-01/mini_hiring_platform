f5bf6cffdfea0adfd276097841b75f92d6772ddc
coding-freak-01 <ankitassuman700@gmail.com>
2025-09-28 23:45:42 +0530

    did updation for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // For local dev (so refreshing /candidates/1 works)
+    historyApiFallback: true,
+  },
+  preview: {
+    // For `vite preview` and deployment preview
+    port: 4173,
+    strictPort: true,
+  }
 })
